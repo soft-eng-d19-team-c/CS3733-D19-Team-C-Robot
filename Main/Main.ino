e259e72d0339ce81af03267a52b022c702513a14
mcburd <mcburd@wpi.edu>
2019-04-13 12:22:46 -0400

    Init pseudocode

---
+++ Main/Main.ino
@@ -0,0 +1,21 @@
+void setup() {
+  Serial.begin(9600);
+  // put your setup code here, to run once:
+  //Get POST
+  //Parde data into array of directon/distance
+  Serial.println(Setup Complete)
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  //Iterate through array
+  //Make each move
+  //Wait until finished
+  //Onto the next 
+
+}
+
+//Helper functions for:
+//driveStraight()
+//turnLeft()
+//turnRight()
