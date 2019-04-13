6ce788c6c73c880ee320e6963551fee646b7e582
mcburd <mcburd@wpi.edu>
2019-04-13 18:54:42 -0400

    Number of moves can be counted. working on populating array of distance and array of direction

--- CS3733_Robot/CS3733_Robot.ino
+++ CS3733_Robot/CS3733_Robot.ino
@@ -1,41 +1,68 @@
+const int max_moves = 100;
+String thePOST;
+int len;
+double distances[max_moves];
+char directions[max_moves];
+int numMoves;
+
 void setup() {
   Serial.begin(9600);
   // put your setup code here, to run once:
   //Get POST
   /*http magic*/
-  String fakePOST = "S,20.0,L,50.0,S,15.4,L,150.0,S,20.0,R,30.0,L,10.0,S,17.0,L,4.5"
-  String thePOST = fakePOST //For now...
-  
+  String fakePOST = "S,20.0,L,50.0,S,15.4,L,150.0,S,20.0,R,30.0,L,10.0,S,17.0,L,4.5";
+  thePOST = fakePOST; //For now...
+  len = thePOST.length();
+  numMoves = count_moves(thePOST);
   //Parse data into array of directon/distance
 
-  int count_chars(const char* thePOST, char ch){
-    
-    int count = 0;
-    int i;
-    int length = strlen(string);
 
-    for (i = 0; i < length; i++)
-    {
-        if (string[i] == ch)
-        {
-            count++;
-        }
-    }
 
-    return count;
-  }
-  
-  Serial.println(Setup Complete)
+  Serial.println("Setup Complete");
 }
+
 
 void loop() {
   // put your main code here, to run repeatedly:
   //Iterate through array
   //Make each move
   //Wait until finished
-  //Onto the next 
+  //Onto the next
+}
+
+//Counts the number of moves contained in the data
+int count_moves(String theData) {
+
+  int count = 0;
+  int i = 0;
+  int s = 0;
+  int moves;
+
+   for (i = 0; i < len; i++)
+    {
+     if (theData.charAt(i) == ',')
+     {
+       count++;
+     }
+    }
+
+  moves = (count - 1) / 2;
+  Serial.println("Moves:" + moves);
 
 }
+
+void populateDistances(){
+
+  for (i = 0; i <= numMoves; i + 2){
+
+    token = strtok(thePOST,",");
+    distances[i] = token;
+    
+  }
+  
+}
+
+
 
 //Helper functions for:
 //driveStraight()
--- README.md
+++ README.md
@@ -3,19 +3,19 @@ Code for the robot
 
 ## POST data format
 Comma-seperated String: "turn, distance, turn, distance..."
+
 turn: String containing either L, R, or S
+
 distance: double containing distance to drive straight in feet
 
-###Example
+### Example
 
 Starting at Hallway 7 Floor L1
 Take a left at Hallway 8 Floor L1
 Continue straight past Elevator K Floor L1
 Take a left at Hallway 9 Floor L1
 Continue straight past Vending Machine 1 L1
-
 Take a right at Hallway 12 Floor L1
-
 Take a left at Hallway 13 Floor L1
 Continue straight past Day Surgery Family Waiting Floor L1
 Take a left at Day Surgery Family Waiting Exit Floor L1
