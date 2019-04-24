316bc2f6c84436dacccf8e631eb9cde5961fa9bb
mcburd <mcburd@wpi.edu>
2019-04-24 02:57:19 -0400

    Cleanup

--- CS3733_Robot/arduino_secrets.h
+++ CS3733_Robot/arduino_secrets.h
@@ -1,10 +1,4 @@
 //#define SECRET_SSID "WPI-Open"
 
-//#define SECRET_SSID  "Router? I hardly know her!"
-//#define SECRET_PASS  "PrettyWifiForAFly"
-
 #define SECRET_SSID "Batman"
 #define SECRET_PASS "hersh1234"
-
-//#define SECRET_SSID "Matt's iPhone"
-//#define SECRET_PASS "ja0qs9rk4i7hm"
